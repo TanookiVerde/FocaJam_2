4d3a9b3f42aae6135763acd61b537b0a0fef4eba
Pedro Nascimento <pedrovitor.marques@gmail.com>
2018-03-03 21:55:59 -0300

    Mapa simples

---
+++ Murder/Assets/Scripts/MapCreator.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapCreator : MonoBehaviour {
+
+	const int size = 8;
+	private int[,] map  = {{0,0,0,0,0,0,0,0},
+						   {0,0,0,0,0,0,0,0},
+						   {0,0,0,0,0,0,0,0},
+						   {0,0,0,0,0,0,0,0},
+						   {0,0,0,0,0,0,0,0},
+						   {0,0,0,0,0,0,0,0},
+						   {0,0,0,0,0,0,0,0},
+						   {0,0,0,0,0,0,0,0}};
+	[SerializeField] private Transform mapParent;
+	[SerializeField] private List<GameObject> tiles;
+
+	private void Start(){
+		InitializeMap();
+	}
+	private void InitializeMap(){
+		float tileSize = tiles[0].GetComponent<SpriteRenderer>().size.x;
+		for(int y = 0; y < size; y++){
+			for(int x = 0; x < size; x++){
+				Vector3 position = new Vector3((float) x,(float) y, 0)*tiles[0].GetComponent<SpriteRenderer>().size.x;
+				var go = Instantiate(tiles[0],position,Quaternion.identity);
+				go.transform.parent = mapParent;
+			}
+		}
+		mapParent.transform.position -= new Vector3(size*tileSize - tileSize*0.5f,size*tileSize - tileSize,0)*0.5f;
+	}
+}
